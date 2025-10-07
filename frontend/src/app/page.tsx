558f594369137f71bd33fc4ceba09cfcf83eb0be
TrailingWhitespace <flash1719@protonmail.com>
2025-10-08 01:26:40 +0530

    display todos and stuff
    display todos in home page, apihealth route, TodoList component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import TodoList from '@/components/todos/TodoList';
 
 export default async function HomePage() {
   return (
     <main>
       <h1>Home dayo</h1>
+      <TodoList />
     </main>
   );
 }
---
+++ frontend/src/components/todos/TodoList.tsx
@@ -0,0 +1,38 @@
+
+import React from 'react';
+
+
+type Todo = {
+  id: number;
+  content: string;
+  completed: boolean;
+  createdAt: string;
+};
+
+// TODO Move this to lib/ 
+async function getTodos(): Promise<Todo[]> {
+  try {
+    const apiUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+    const res = await fetch(`${apiUrl}/api/todos`, { cache: 'no-store' });
+    if (!res.ok) throw new Error(`Failed to fetch`);
+    return res.json();
+  } catch (error) {
+    console.error("Fetch Error:", error);
+    return [];
+  }
+}
+
+export default async function TodoList() {
+  const todos = await getTodos();
+  let error: string | null = null;
+
+  return (
+    <ul className="space-y-4">
+      {todos.length > 0 ? (
+        todos.map((todo) => <li key={todo.id}>{todo.content}</li>)
+      ) : (
+        <p className="text-center text-gray-500">No todos</p>
+      )}
+    </ul>
+  );
+}
